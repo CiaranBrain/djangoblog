396ef7eff8c1ce98cb234e253b483282a2d30811
Ciaran Brain <ciaranb333@gmail.com>
2024-02-23 23:01:43 +0000

    blog added

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lwn@tum0gg+y@wekcwa)=6iok3el2)$*so3lj99k#d*23)n_#a'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciaranbrain-djangoblog-gzdygwgvws2.ws-eu108.gitpod.io','127.0.0.1:8000','localhost','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciaranbrain-djangoblog-gzdygwgvws2.ws-eu108.gitpod.io','127.0.0.1:8000','localhost','.herokuapp.com','https://8000-ciaranbrain-djangoblog-gzdygwgvws2.ws-eu108.gitpod.io' ]
 
 
 # Application definition
@@ -88,6 +88,13 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://django-blog-testt-73e5953bdd29.herokuapp.com/", 
+    "https://8000-ciaranbrain-djangoblog-gzdygwgvws2.ws-eu108.gitpod.io"
+
+]
 
 
 # Password validation
