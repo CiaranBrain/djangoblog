839f1c998a2c7710dca19cea73547d741f1392e7
Ciaran Brain <ciaranb333@gmail.com>
2024-02-24 00:37:47 +0000

    added updatetime

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ciaranbrain-djangoblog-gzdygwgvws2.ws-eu108.gitpod.io','127.0.0.1:8000','localhost','.herokuapp.com','https://8000-ciaranbrain-djangoblog-gzdygwgvws2.ws-eu108.gitpod.io' ]
 
