80434900f26786cdfc3113112c8f1b4534c15745
Ciaran Brain <ciaranb333@gmail.com>
2024-02-25 01:24:25 +0000

    added metas and methods in comments and posts

--- blog/models.py
+++ blog/models.py
@@ -16,9 +16,22 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["created_on", "author"]
+
+    def __str__(self):
+        return f"The title of this post is - {self.title}"
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")  
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")  
     body = models.TextField()
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+  
